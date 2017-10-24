e2123242e54aecee013a6dc043d644a94bf6e3bd
damacus <dan.webb@damacus.io>
2017-10-24 10:38:07 +0100

    Meh

--- tests/controls/docker.rb
+++ tests/controls/docker.rb
@@ -7,7 +7,7 @@ control 'container-name' do
   impact 0.7
   title 'Names & Tags'
   desc 'Should be tagged php-composer-builder'
-  describe docker_image('damacus/php-composer-builder') do
+  describe docker_image('damacus/php-composer-builder:latest') do
     it { should exist }
     its('repo') { should match /damacus\/php-composer-builder/ }
     its('tag') { should eq 'latest' }
