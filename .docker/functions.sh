d477c10b3cd541d78f30244c49bf4c62af9c8bdd
Dan Webb <dan.webb@damacus.io>
2018-02-09 11:21:24 +0000

    Allow users to overide ORG and script version

--- .docker/functions.sh
+++ .docker/functions.sh
@@ -2,9 +2,15 @@
 set +o pipefail
 
 export DESCRIPTION="Docker container composer on PHP 7.2"
+if [[ -z $ORG ]];then 
+  ORG="damacus"
+fi
+if [[ -z $BUILD_SCRIPT_VER ]];then
+	BUILD_SCRIPT_VER='master'
+fi
 
 if ! [[ -e .docker/external_functions.sh ]];then
-  wget -O ".docker/external_functions.sh" "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh"
+  wget -O ".docker/external_functions.sh" "https://raw.githubusercontent.com/${ORG}/docker-build-scripts/${BUILD_SCRIPT_VER}/functions.sh"
 fi
 
 source ".docker/external_functions.sh"
